e800bc29ce23d536d463294448d664505c00874b
keshri95 <keshri.ashish1@gmail.com>
2022-08-29 15:30:13 +0530

    UI of color-flipper

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// colors
+const colrsName = [];
+
+const buttonClick = document.querySelector('#clrBtn')
+
+buttonClick.addEventListener("click", colorChange)
+
+function colorChange(){
+    console.log('colors will change random');
+}
