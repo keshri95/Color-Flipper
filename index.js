71db2ce7a8bdb63be8c8a7e69d4dced97a20ea67
keshri95 <keshri.ashish1@gmail.com>
2022-08-29 23:25:45 +0530

    colors added dynamic

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 
 // colors
-const colrsName = [];
+const colrsNames = ['red', 'green', 'blue', 'yellow', 'purple', '#ccc', '#000'];
 
+// span color name
+const color = document.querySelector('#color')
+
+// button event 
 const buttonClick = document.querySelector('#clrBtn')
 
-buttonClick.addEventListener("click", colorChange)
+buttonClick.addEventListener("click", () =>{
 
-function colorChange(){
-    console.log('colors will change random');
+
+// function colorChange(){
+
+    const randomClr = randomNumber()
+        document.body.style.backgroundColor = colrsNames[randomClr]
+        color.textContent = colrsNames[randomClr]
+})
+
+// random number generator 0-1
+const randomNumber =() =>{
+    return Math.floor(Math.random() * colrsNames.length)
 }
